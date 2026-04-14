812c215a5148feeaea3b4fe62797efb4823ab4ff
skedia16 <shreyakedia99@gmail.com>
2026-04-14 16:34:04 +0530

    Update OptimalStopping.tsx with proper TypeScript typing

---
+++ src/components/OptimalStopping.tsx
@@ -0,0 +1,19 @@
+// Updated OptimalStopping.tsx with TypeScript typing
+import React from 'react';
+
+interface OptimalStoppingProps {
+  // Define your props here with proper TypeScript typing
+  // For example:
+  value: number;
+  options: string[];
+}
+
+const OptimalStopping: React.FC<OptimalStoppingProps> = (props) => {
+  return (
+    <div>
+      {/* Component logic goes here */}
+    </div>
+  );
+};
+
+export default OptimalStopping;
